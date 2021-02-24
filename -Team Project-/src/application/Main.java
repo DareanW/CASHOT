fdff16afe9b33f0deace59976954c93dc528301e
Darean Wilde <63678621+Ean173@users.noreply.github.com>
2021-02-24 09:26:00 -0600

    Update Main.java

--- -Team Project-/src/application/Main.java
+++ -Team Project-/src/application/Main.java
@@ -5,7 +5,6 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
-
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
