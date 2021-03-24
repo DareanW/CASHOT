2661821da04887b65c7890d994c1a868d2b9f3c0
Darean Wilde <grl167@my.utsa.edu>
2021-03-24 10:57:58 -0500

    System.java

---
+++ CASHOT/src/application/model/System.java
@@ -0,0 +1,17 @@
+package application.model;
+import java.io.IOException;
+import java.util.ArrayList;
+public class System {
+	
+	private int ID;
+	private ArrayList<Receipt> receipt;
+	private ArrayList<Employee> employee;
+	
+	
+	public System {
+		public void loadEmployee() throws IOException {
+	}
+		private void addEmployee(Employee employees) {
+			employees.add(employee);
+		}
+}
