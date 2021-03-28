3241ff4c83342d847f7191d54ea4a9203567a59e
Darean Wilde <grl167@my.utsa.edu>
2021-03-28 16:22:05 -0500

    Made a few minor changes

--- CASHOT/src/application/model/Employee.java
+++ CASHOT/src/application/model/Employee.java
@@ -1,55 +1,69 @@
 package application.model;
 import java.util.ArrayList;
 import java.util.Date;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Employee {
-	private String employeeName;
-	private String userName;
-	private String employeePassword;
-	private static int ID;
-	private static ArrayList<Order> order;
+private String employeeName;
+private String userName;
+private String employeePassword;
+private static int ID;
+private static ArrayList<Order> order;
+
+public Employee(String employeeName, String userName, String employeePassword, int ID){
+	this.employeeName = employeeName;
+	this.userName = userName;
+	this.employeePassword = employeePassword;
+	this.ID = ID;
+	this.order = new ArrayList<Order>();
+}
+
+
+public String getEmployeeName() {
+	return employeeName;
+}
+public void setEmployeeName(String employeeName) {
+	this.employeeName = employeeName;
+}
+public String getUserName() {
+	return userName;
+}
+public void setUserName(String userName) {
+	this.userName = userName;
+}
+public String getEmployeePassword() {
+	return employeePassword;
+}
+public void setEmployeePassword(String employeePassword) {
+	this.employeePassword = employeePassword;
+}
+public static int getID() {   //changed to static
+	return ID;
+}
+public void setID(int iD) {
+	ID = iD;
+}
+public static ArrayList<Order> getOrder() {
+	return order;
+}
+public void setOrder(ArrayList<Order> order) {
+	this.order = order;
+}
+public static int hashNum(String user){
+	int num;
+	return (num);
+}
+
+public void add(Employee employee) {
+ 
 	
-	public Employee(String employeeName, String userName, String employeePassword, int ID){
-		this.employeeName = employeeName;
-		this.userName = userName;
-		this.employeePassword = employeePassword;
-		this.ID = ID;
-		this.order = new ArrayList<Order>();
-	}
-	
-	
-	public String getEmployeeName() {
-		return employeeName;
-	}
-	public void setEmployeeName(String employeeName) {
-		this.employeeName = employeeName;
-	}
-	public String getUserName() {
-		return userName;
-	}
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	public String getEmployeePassword() {
-		return employeePassword;
-	}
-	public void setEmployeePassword(String employeePassword) {
-		this.employeePassword = employeePassword;
-	}
-	public static int getID() { //changed to static
-		return ID;
-	}
-	public void setID(int iD) {
-		ID = iD;
-	}
-	public static ArrayList<Order> getOrder() { //changed to static
-		return order;
-	}
-	public void setOrder(ArrayList<Order> order) {
-		this.order = order;
-	}
-	
-	public String toString(){
-		return this.ID + " " + this.employeeName + " " + this.userName;
-	}
+}
 }
---
+++ CASHOT/src/application/model/System.java
@@ -0,0 +1,19 @@
+package application.model;
+import java.io.IOException;
+import java.util.ArrayList;
+public class System {
+	
+	private int ID;
+	private ArrayList<Receipt> receipt;
+	private ArrayList<Employee> employee;
+	
+	
+
+		public void loadEmployee() throws IOException {
+			return;
+	}
+		private void addEmployee(Employee employees) {
+			employees.add(employee);
+		}
+	}
+
