8b1737553a80c248cc3c14ee27f277d5f4d90e30
Majerus <HP@LAPTOP-7EJIUHQ6.lan>
2021-04-09 13:08:28 -0500

    editItems layout

--- CASHOT/src/application/controller/EditItemsController.java
+++ CASHOT/src/application/controller/EditItemsController.java
@@ -1,5 +1,28 @@
 package application.controller;
 
-public class EditItemsController {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.NumberFormat;
 
+import application.model.CashotSystem;
+import application.model.Item;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class EditItemsController implements EventHandler {
+
+	@Override
+	public void handle(Event event) {
+		
+		
+	}
+	
 }
